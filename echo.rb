5f5da49d7b661ff1e4ffcb9e67ffac278b2b45ad
Jamin Guy <jaminguy@gmail.com>
2011-05-21 17:42:06 -0500

    added / method

--- echo.rb
+++ echo.rb
@@ -1,5 +1,14 @@
 require 'rubygems'
 require 'sinatra'
+require 'json'
+
+get '/' do
+  output = "<table border-spacing='0' border-width='1' border-style='solid'>"
+  request.env.select {|k,v| k.match("^HTTP.*")}.each do |header|
+    output += "<tr><td align='right'>#{header[0].split('_',2)[1]}:</td><td>#{header[1]}</td></tr>"
+  end
+  output += "</table>"
+end
 
 get '/headers' do
   output = "<table border-spacing='0' border-width='1' border-style='solid'>"
@@ -7,6 +16,10 @@ get '/headers' do
     output += "<tr><td align='right'>#{header[0].split('_',2)[1]}:</td><td>#{header[1]}</td></tr>"
   end
   output += "</table>"
+end
+
+get '/headers.json' do
+  request.env.select {|k,v| k.match("^HTTP.*")}.to_json
 end
 
 get '/user_agent' do
