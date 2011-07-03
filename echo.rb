89619ce21eec7d1ca2547d443d377c8e4aee7a02
Jamin Guy <jaminguy@gmail.com>
2011-07-03 14:26:58 -0500

    refactored html generation to a helper method

--- echo.rb
+++ echo.rb
@@ -2,20 +2,22 @@ require 'rubygems'
 require 'sinatra'
 require 'json'
 
+helpers do
+    def html_for_headers()
+      output = "<table border-spacing='0' border-width='1' border-style='solid'>"
+      request.env.select {|k,v| k.match("^HTTP.*")}.each do |header|
+        output += "<tr><td align='right'>#{header[0].split('_',2)[1]}:</td><td>#{header[1]}</td></tr>"
+      end
+      output += "</table>"
+    end
+end
+
 get '/' do
-  output = "<table border-spacing='0' border-width='1' border-style='solid'>"
-  request.env.select {|k,v| k.match("^HTTP.*")}.each do |header|
-    output += "<tr><td align='right'>#{header[0].split('_',2)[1]}:</td><td>#{header[1]}</td></tr>"
-  end
-  output += "</table>"
+  html_for_headers()
 end
 
 get '/headers' do
-  output = "<table border-spacing='0' border-width='1' border-style='solid'>"
-  request.env.select {|k,v| k.match("^HTTP.*")}.each do |header|
-    output += "<tr><td align='right'>#{header[0].split('_',2)[1]}:</td><td>#{header[1]}</td></tr>"
-  end
-  output += "</table>"
+   html_for_headers()
 end
 
 get '/headers.json' do
