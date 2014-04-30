d42cbd1c360b26da7094bf415f6460b2f071c74a
Jamin Guy <jaminguy@gmail.com>
2014-04-30 13:34:24 -0500

    Add sorting to json conversion

--- public/json_to_html.js
+++ public/json_to_html.js
@@ -1,8 +1,24 @@
-
+function stringifyJSON(obj){
+    keys = [];
+    if(obj){
+        for(var key in obj){
+            keys.push(key);
+        }
+    }
+    keys.sort();
+    var tObj = {};
+    var key;
+    for(var index in keys){
+        key = keys[index];
+        tObj[ key ] = obj[ key ];
+    }
+    return JSON.stringify(tObj, undefined, 2);
+}
 
 function syntaxHighlight(json) {
     if (typeof json != 'string') {
-         json = JSON.stringify(json, undefined, 2);
+      //console.log("converting object to json string");
+      json = stringifyJSON(json)
     }
     json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
     return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
